608f16b65652fa5c9b823d90be6c230f47a89ab0
Akash Ramesh <akashsumi0007@gmail.com>
2023-12-12 09:29:59 +0530

    string 2 problem

--- Html/index1.js
+++ Html/index1.js
@@ -24,3 +24,47 @@
 // reduceRight((acc,char) => acc + char, "")
 
 // console.log(reversedString)
+
+
+//2......check if string is a palindrome.
+//2.1 using the reverse Method;
+//first reverse a string and compare it to the 
+//original string
+// function isPalindrome(str){
+//     const reversedStr = str.split("").reverse().join("")
+//     return str === reversedStr
+// }
+
+// console.log(isPalindrome("madam"))
+
+///////2.2 using a for loop iterate through the strings
+//from both ends and compare them.
+
+
+// function isPalindrome(str){
+//     const len  = str.length;
+
+//     for(let i=0; i< len/2; i++){
+//         if(str[i] !== str[len - 1 -i]){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// const str = "radar"
+// const result = isPalindrome(str)
+
+// console.log(result)
+
+
+/////////2.3 Using Array Methods.
+// function isPalindrome(str){
+//     const cleanStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g,"")
+//     const reversedStr = cleanStr.split("").reverse().join("")
+//     return cleanStr === reversedStr;
+// }
+
+// const result = isPalindrome("madam")
+
+// console.log(result)
